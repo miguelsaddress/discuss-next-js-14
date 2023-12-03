a9333706213cea512ab8afdbf4c498544deb5d55
Miguel Angel Moreno Armenteros <1411774+miguelsaddress@users.noreply.github.com>
2023-12-03 09:25:04 +0100

    Setup NextAuth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,4 @@
+// handles requests between GH server and ours
+
+// API request handlers to talk to outside APIs
+export { GET, POST } from '@/auth';
---
+++ src/auth.ts
@@ -0,0 +1,39 @@
+import NextAuth from 'next-auth';
+import GitHub from 'next-auth/providers/github';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { db } from '@/db';
+
+const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+  throw new Error('Missing Github OAuth credentials');
+}
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signOut,
+  signIn,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    GitHub({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    // called whenever we verify who our user is in the app
+    // mey be unnecessary, depending on a bug resolution in nextAuth
+    // In the bug, the user does not properly get the id assigned to it
+    async session({ session, user }) {
+      if (session && user) {
+        if (!session.user) {
+          session.user = { id: '' };
+        }
+        session.user.id = user.id;
+      }
+
+      return session;
+    },
+  },
+});
