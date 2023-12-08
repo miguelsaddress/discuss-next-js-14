d55e1dcd2cdc924b6c0d745169879bb8a8f40fa6
Miguel Angel Moreno Armenteros <1411774+miguelsaddress@users.noreply.github.com>
2023-12-08 20:11:40 +0100

    clean the home page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,3 @@
-import { auth } from '@/auth';
-import Profile from '@/components/profile';
-import { signIn, signOut } from '@/server-actions';
-import { Button } from '@nextui-org/react';
-
-export default async function Home() {
-  const signInAction = signIn.bind(null, 'github');
-  const session = await auth();
-
-  console.log({ session, user: session?.user });
-  const loggedIn = session?.user;
-
-  return (
-    <div>
-      <Profile />
-      {loggedIn ? (
-        <form action={signOut}>
-          <Button type="submit">Sign Out</Button>
-        </form>
-      ) : (
-        <form action={signInAction}>
-          <Button type="submit">Sign In</Button>
-        </form>
-      )}
-    </div>
-  );
+export default function Home() {
+  return <h1>Home Page</h1>;
 }
