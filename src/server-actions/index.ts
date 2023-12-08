463bcfa5151b16cc56b93123602f2cd8aff6a58c
Miguel Angel Moreno Armenteros <1411774+miguelsaddress@users.noreply.github.com>
2023-12-08 18:59:51 +0100

    add skeleton for the needed server actions that change data

--- src/server-actions/index.ts
+++ src/server-actions/index.ts
@@ -1,20 +1,5 @@
-'use server';
-
-import * as auth from '@/auth';
-import { db } from '@/db';
-import { OAuthProviderType } from 'next-auth/providers';
-
-type SigInProvider = OAuthProviderType;
-
-export async function signIn(provider: SigInProvider = 'github') {
-  const user = await db.user.findFirst();
-  const account = await db.account.findFirst();
-  console.log(user);
-  console.log(account);
-
-  return auth.signIn(provider);
-}
-
-export async function signOut() {
-  return auth.signOut();
-}
+export { signIn } from './sign-in';
+export { signOut } from './sign-out';
+export { createComment } from './create-comment';
+export { createPost } from './create-post';
+export { createTopic } from './create-topic';
